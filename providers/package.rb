5451c8ab00d59d0e788f2b2acce9e6eb1077a409
Gerhard Lazu <gerhard@lazu.co.uk>
2012-08-20 15:05:25 +0100

    version matching fix when updating installed packages

--- providers/package.rb
+++ providers/package.rb
@@ -1,13 +1,13 @@
 action :add do
   package_options = new_resource.options
   package_options += ' -o Dpkg::Options::="--force-confold"' if new_resource.keep_conf
-  package_status = new_resource.freeze ? '^h' : '^i'
+  package_status = new_resource.freeze ? 'h' : 'i'
 
   package new_resource.name do
     action :install
     version new_resource.version
     options package_options
-    only_if "[ $(dpkg -l #{new_resource.name} 2>&1 | grep #{new_resource.version} | grep -c '#{package_status}[ic] ') = 0 ]"
+    only_if "[ $(dpkg -l #{new_resource.name} 2>&1 | grep #{new_resource.version}.* | grep -c '^#{package_status}[ic] ') = 0 ]"
   end
 
   # Legacy, calling the hold/unhold action is the recommended way
