e0dd21153c718eb630be7b9ee29455c87299b584
Gerhard Lazu <gerhard@lazu.co.uk>
2012-08-29 12:44:03 +0100

    default apt_repository compatibility

--- resources/repository.rb
+++ resources/repository.rb
@@ -7,6 +7,9 @@ attribute :uri,            :kind_of => String
 attribute :sources,        :kind_of => Array,   :default => ["deb"]
 attribute :distributions,  :kind_of => Array,   :default => [node[:lsb][:codename]]
 attribute :components,     :kind_of => Array,   :default => ["main"]
+# Compatibility with the default apt_repository provider
+# No used anywhere, :distributions is more versatile
+attribute :distribution
 
 def initialize(*args)
   super
